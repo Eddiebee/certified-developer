ac64eebb69600dc35f5a22a9f817eb5acdc1ccdf
Eddie Otudor <edbeeotudormail.com>
2024-04-09 01:38:42 +0000

    update: add pressure parameter to test function

--- MLOps_Professional/lab1/sample/maintenance.py
+++ MLOps_Professional/lab1/sample/maintenance.py
@@ -1,18 +1,20 @@
-# maitnenace test business logic
+# maintenance test business logic
 
-def test_maintenance(temperature:int):
+def test_maintenance(temperature:int, pressure:int):
     """_summary_
 
     Parameters
     ----------
     temperature : int
         test parameter for temperature sensor readings
+    pressure : int
+        parameter for hydraulic pressure 
 
     Returns
     -------
     str
-        'Approved' or 'Denied' based on temperature readings
+        'Approved' or 'Denied' based on temperature or hydraulic pressure readings
     """
-    maintenance_status = 'Needs Maintenance' if temperature > 50 else 'No Maintenance Required'
+    maintenance_status = 'Needs Maintenance' if (temperature and temperature > 50) or (pressure and pressure > 2000) else 'No Maintenance Required'
     
     return maintenance_status
